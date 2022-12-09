993dbecdf5dc0b622b17fd726297a82375cb7517
Bruno Paulino <bruno@bpaulino.com>
2022-12-09 14:51:21 +0100

    Use custom workers for multi-threaded web server

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,11 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-}
-
-pub struct Worker {
-    id: String,
-    thread: thread::JoinHandle<()>,
-}
-
-impl Worker {
-    pub fn new(id: String) -> Self {
-        let thread_id = id.clone();
-        // if we want that to really be production-grade
-        // we should consider using std::thread::Builder instead.
-        let handle = thread::spawn(move || {
-            println!("Worker with id '{}' starting...", thread_id);
-        });
-
-        Self { id, thread: handle }
-    }
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -33,16 +19,25 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0, "Can't have an empty thread pool buddy...");
 
+        let (sender, receiver) = mpsc::channel();
+
+        // Rust default implementation is based on
+        // Multiple producers (sender), Single Consumer (receiver).
+        // We can't share the receiver with multiple threads by default
+        // by cloning it. We must use atomics here to proper reference-count
+        // and a mutex to control access when mutating it (consuming new messages).
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for index in 0..size {
             // create some threads and add to the pool
             let id = format!("rust-web-server-worker-{}", index);
-            let w = Worker::new(id);
+            let w = Worker::new(id, Arc::clone(&receiver));
             workers.push(w);
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -55,5 +50,42 @@ impl ThreadPool {
         // we don't know how long this closure will live.
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: String,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: String, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread_id = id.clone();
+        // if we want that to really be production-grade
+        // we should consider using std::thread::Builder instead.
+        let handle = thread::spawn(move || {
+            println!("Worker with id '{}' starting...", thread_id);
+            loop {
+                // The cool thing about Rust here is that
+                // any intermediate values help during the `let` expression
+                // on the right side are dropped once the let statement ends.
+                //
+                // So the lock is released immediately released once the `recv`
+                // call gets its message.
+                //
+                // When the worker is executing the `job` call, other threads
+                // can already consume more messages in the channel.
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("[{}] Got a job. executing...", thread_id);
+                job();
+                println!("[{}] Job done", thread_id);
+            }
+        });
+
+        Self { id, thread: handle }
     }
 }
