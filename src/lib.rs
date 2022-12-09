9d076126d43b63879324d65ded6fdd747ba09e32
Bruno Paulino <bruno@bpaulino.com>
2022-12-09 14:22:27 +0100

    Use workers to manage thread instances

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,26 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+
+pub struct Worker {
+    id: String,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: String) -> Self {
+        let thread_id = id.clone();
+        // if we want that to really be production-grade
+        // we should consider using std::thread::Builder instead.
+        let handle = thread::spawn(move || {
+            println!("Worker with id '{}' starting...", thread_id);
+        });
+
+        Self { id, thread: handle }
+    }
+}
 
 impl ThreadPool {
     /// Create a ThreadPool.
@@ -10,7 +32,17 @@ impl ThreadPool {
     /// The `new` fn will panic if the size is zero.
     pub fn new(size: usize) -> Self {
         assert!(size > 0, "Can't have an empty thread pool buddy...");
-        Self
+
+        let mut workers = Vec::with_capacity(size);
+
+        for index in 0..size {
+            // create some threads and add to the pool
+            let id = format!("rust-web-server-worker-{}", index);
+            let w = Worker::new(id);
+            workers.push(w);
+        }
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
